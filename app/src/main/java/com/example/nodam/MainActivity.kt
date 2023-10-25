d02835e8f1f90d4fd31560975902b00bae3fdb87
Choi Hyo Seok <tennfin1@gmail.com>
2023-10-25 22:38:11 +0900

    Before develop

--- app/src/main/java/com/example/nodam/MainActivity.kt
+++ app/src/main/java/com/example/nodam/MainActivity.kt
@@ -7,5 +7,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fragment = MainHomeFragment() // MyFragment는 사용자가 만든 프래그먼트 클래스입니다
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
     }
 }
--- app/src/main/java/com/example/nodam/MainHomeFragment.kt
+++ app/src/main/java/com/example/nodam/MainHomeFragment.kt
@@ -1,4 +1,18 @@
 package com.example.nodam
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 
-class MainHomeFragment {
+class MainHomeFragment : Fragment() {
+
+    // 프래그먼트가 화면에 표시될 때 호출되는 메소드
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // 프래그먼트의 레이아웃을 인플레이트하고 반환합니다.
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
 }
